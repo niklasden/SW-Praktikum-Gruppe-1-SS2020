05d878b76273bf9004ead5e06c6e21f4e0a43bf6
Niklas Denneler <niklas.denneler@gmail.com>
2020-06-02 16:55:16 +0200

    Implemented the frontend logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,156 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//** Start React Router Import **/
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+//** End React Router Import **/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//** Start React Router Import **/
+import { Container, ThemeProvider, CssBaseline } from '@material-ui/core';
+//** End React Router Import **/
+
+//** Start Firebase Import **/
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+//** End Firebase Import **/
+
+//** Start Layout Import **/
+import Header from './components/layout/Header';
+import Theme from './Theme';
+import SignIn from './components/pages/SignIn';
+import LoadingProgress from './components/dialogs/LoadingProgress';
+import ContextErrorMessage from './components/dialogs/ContextErrorMessage';
+//** End Layout Import **/
+
+class App extends React.Component {
+  #firebaseConfig = {
+    apiKey: "AIzaSyDU8V5EtAYwsu4w9hlPNdqnGlTJqlugSIg",
+    authDomain: "sw-praktikum-gruppe-1-ss2020.firebaseapp.com",
+    databaseURL: "https://sw-praktikum-gruppe-1-ss2020.firebaseio.com",
+    projectId: "sw-praktikum-gruppe-1-ss2020",
+    storageBucket: "sw-praktikum-gruppe-1-ss2020.appspot.com",
+    messagingSenderId: "958039771079",
+    appId: "1:958039771079:web:3fdba7615bc4ca25b93be2"
+  };
+  constructor (props) {
+    super(props);
+        
+    // Init an empty state
+    this.state = {
+      currentUser: null,
+      appError: null,
+      authError: null,
+      authLoading: false
+    };
+  }
+  /** 
+	 * Create an error boundary for this app and recieve all errors from below the component tree.
+	 * 
+	 * @See See Reacts [Error Boundaries](https://reactjs.org/docs/error-boundaries.html)
+ 	 */
+	static getDerivedStateFromError(error) {
+		// Update state so the next render will show the fallback UI.
+		return { appError: error };
+	}
+
+	/** Handles firebase usres logged in state changes  */
+	handleAuthStateChange = user => {
+		if (user) {
+			this.setState({
+				authLoading: true
+			});
+			// The user is signed in
+			user.getIdToken().then(token => {
+				// Add the token to the browser's cookies. The server will then be
+				// able to verify the token against the API.
+				// SECURITY NOTE: As cookies can easily be modified, only put the
+				// token (which is verified server-side) in a cookie; do not add other
+				// user information.
+				document.cookie = `token=${token};path=/`;
+
+				// Set the user not before the token arrived 
+				this.setState({
+					currentUser: user,
+					authError: null,
+					authLoading: false
+				});
+			}).catch(e => {
+				this.setState({
+					authError: e,
+					authLoading: false
+				});
+			});
+		} else {
+			// User has logged out, so clear the id token
+			document.cookie = 'token=;path=/';
+
+			// Set the logged out user to null
+			this.setState({
+				currentUser: null,
+				authLoading: false
+			});
+		}
+	}
+  /** 
+   * Handles the sign in request of the SignIn component uses the firebase.auth() component to sign in.
+	 * @see See Google [firebase.auth()](https://firebase.google.com/docs/reference/js/firebase.auth.Auth)
+	 * @see See Google [firebase.auth().signInWithRedirect](https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signinwithredirect)
+	 */
+	handleSignIn = () => {
+		this.setState({
+			authLoading: true
+		});
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithRedirect(provider);
+	}
+
+	/**
+	 * Lifecycle method, which is called when the component gets inserted into the browsers DOM.
+	 * Initializes the firebase SDK.
+	 * 
+	 * @see See Googles [firebase init process](https://firebase.google.com/docs/web/setup)
+	 */
+	componentDidMount() {
+		firebase.initializeApp(this.#firebaseConfig);
+		firebase.auth().languageCode = 'en';
+		firebase.auth().onAuthStateChanged(this.handleAuthStateChange);
+	}
+    render(){
+      const { currentUser, appError, authError, authLoading } = this.state;
+
+      return (
+        <ThemeProvider theme={Theme}>
+          {/* Global CSS reset and browser normalization. CssBaseline kickstarts an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Router basename={process.env.PUBLIC_URL}>
+            <Container maxWidth='md'>
+              <Header user={currentUser} />
+              {
+                // Is a user signed in?
+                currentUser ?
+                  <>
+                    {/* Here should the redirects go */}
+                    <Redirect from='/' to='' />
+                    <Route exact path='/'>
+                    </Route>
+                
+                  </>
+                  :
+                  // else show the sign in page
+                  <>
+                    <Redirect to='/index.html' />
+                    <SignIn onSignIn={this.handleSignIn} />
+                  </>
+              }
+              <LoadingProgress show={authLoading} />
+              <ContextErrorMessage error={authError} contextErrorMsg={`Something went wrong during sighn in process.`} onReload={this.handleSignIn} />
+              <ContextErrorMessage error={appError} contextErrorMsg={`Something went wrong inside the app. Please reload the page.`} />
+            </Container>
+          </Router>
+        </ThemeProvider>
+      );
+    }
 }
 
 export default App;
