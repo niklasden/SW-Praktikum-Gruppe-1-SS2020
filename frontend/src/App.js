2dc78a32b3734fe08f950d516ac30a6c0eedfb5d
Christpoher Böhm <exsivi.project@gmail.com>
2020-06-03 21:36:26 +0200

    installed material-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
   return (
       <Router>
         <div>
+          {/* TODO: move to own header component */}
           <nav>
             <ul>
               <li>
--- frontend/src/components/pages/AboutPage.js
+++ frontend/src/components/pages/AboutPage.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 export class AboutPage extends Component {
   render(){
     return (
-      <h2>About</h2>
+      <div>
+        <h2>About</h2>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
+
     )
   }
 }
