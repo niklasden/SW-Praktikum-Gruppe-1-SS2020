2eb47e2df3d9a73b546fb5919d3198cd9b88188d
Kevin Eberhardt <kevin96e@kevins-mbp.speedport.ip>
2020-06-06 12:11:31 +0200

    deleted non used items

--- frontend/src/components/pages/AboutPage.js
+++ frontend/src/components/pages/AboutPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import GridList from '@material-ui/core/GridList';
--- frontend/src/components/pages/Header.js
+++ frontend/src/components/pages/Header.js
@@ -3,12 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Menu from './Menu'
-import {
-    Link
-  } from 'react-router-dom';
 import ProfileDropDown from '../dialogs/ProfileDropDown';
 
 const useStyles = makeStyles((theme) => ({
