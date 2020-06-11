ff7c69da514a7c605db24c3abcdba62da4d77017
Christpoher Böhm <exsivi.project@gmail.com>
2020-06-11 16:31:18 +0200

    added retailer bo and retailer mapper

--- frontend/src/components/pages/RetailerPage.js
+++ frontend/src/components/pages/RetailerPage.js
@@ -5,7 +5,6 @@ import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Container from '@material-ui/core/Container/Container'
 
-
 const RETAILERS = [
   {
     id: 'retailer1', 
---
+++ src/server/bo/Retailer.py
@@ -0,0 +1,26 @@
+from server.bo import BusinessObject as bo
+
+""" A single Retailer
+@author Christopher Böhm
+"""
+class Retailer (bo.BusinessObject):
+    def __init__(self):
+        super().__init__(self)
+        self._name = None
+        self._location = None
+
+    def get_name(self):
+        self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_location(self):
+        return self._location
+
+    def set_location(self, location):
+        self._location = location
+
+    def __str__(self):
+        return "Retailer: {}, name: {}".format(self.get_id(), self._name)
+
