9645d480198c401ea23a703f48393447e7009288
Kevin Eberhardt <kevin96e@kevins-mbp.speedport.ip>
2020-06-15 17:32:28 +0200

    changed HeaderButton

--- frontend/src/components/layout/HeaderButton.js
+++ frontend/src/components/layout/HeaderButton.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
-// import IconButton from './IconButton';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
@@ -48,7 +46,7 @@ const styles = theme => ({
   },
 });
 /**
- * Displays an icon button as designed in figma
+ * Displays an header button as designed in figma
  * 
  * @author [Kevin Eberhardt](https://github.com/kevin-eberhardt)
  * 
@@ -56,7 +54,6 @@ const styles = theme => ({
 class HeaderButton extends Component {
     render(){
       const { classes } = this.props;
-  
       return (
         <Grid container className={classes.root} alignItems="center">
             <Grid item md={4} className={classes.iconsItem} style={{borderRight: '1px solid #ccc'}}>
@@ -66,12 +63,6 @@ class HeaderButton extends Component {
               <IconButton className={classes.button2}>
                 <ShoppingCartIcon />
               </IconButton>
-              {/* <IconButton>
-                <ShoppingCartIcon />
-              </IconButton>
-              <IconButton>
-                <FormatListBulletedIcon />
-              </IconButton> */}
             </Grid>
             <Grid item md={8} className={classes.item} >
               <Typography variant="h5" className={classes.heading}>iKaufa</Typography>
@@ -80,10 +71,6 @@ class HeaderButton extends Component {
         </Grid>
       );
     }
-}
-
-HeaderButton.propTypes = {
-  icon: PropTypes.string,
 }
 
 export default withStyles(styles)(HeaderButton);
