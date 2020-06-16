385f021e7601116435f8dade3041c87d1a318ab1
Pia Schmid <ps123@hdm-stuttgart.de>
2020-06-16 13:39:42 +0200

    implemented Searchbar item

---
+++ frontend/src/components/layout/Searchbar.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import { withStyles } from "@material-ui/styles";
+import Box from '@material-ui/core/Box'
+
+/**
+ * Displays an icon button as designed in figma
+ * 
+ * @author [Pia Schmid](https://github.com/PiaSchmid)
+ * 
+*/
+
+const styles = theme => ({
+  root: {
+    backgroundColor: "#fafafa",
+    borderRadius: 10,
+    width: "292px",
+    height: "48px"
+  },
+
+  searchIcon: {
+    paddingLeft: "16px",
+    paddingTop: "12px",
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  inputInput: {
+    paddingLeft: "64px",
+    paddingTop: "15px",
+    fontSize: '18px',
+    fontFamily: 'Roboto'
+    
+  }
+});
+
+class Searchbar extends Component {
+  render() {
+    return (
+      <Box 
+        border={1} 
+        borderColor='#F2F2F2'
+        borderRadius={10}
+        style={{display: 'inline-block'}}
+      >
+      <div>
+        <div className={this.props.classes.root}>
+          <div className={this.props.classes.searchIcon}>
+            <SearchIcon style={{ color: "#00BCD4" }} fontSize="medium" />
+          </div>
+
+          <InputBase
+            placeholder="search…"
+            classes={{
+              input: this.props.classes.inputInput
+            }}
+            inputProps={{ "aria-label": "search" }}
+          />
+        </div>
+      </div>
+    </Box>
+
+    );
+  }
+}
+
+export default withStyles(styles)(Searchbar);
