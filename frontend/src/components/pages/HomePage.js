c257d0e3db4174aaa18e24352fcdbdc4d9967d90
JuliusJacobitz <julius@jacobitz.de>
2020-06-16 22:23:41 +0200

    added Article.js

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '../layout/IconButton';
 import HeaderButton from '../layout/HeaderButton';
+import Article from '../layout/Article'
 
 export class HomePage extends Component {
   render(){
@@ -9,6 +10,9 @@ export class HomePage extends Component {
         <h2>Home</h2>
         <HeaderButton />
         <Button />
+        <Article imgsrc="https://upload.wikimedia.org/wikipedia/de/8/8b/Brights_icon_100x100.gif" itemname="test"/>
+        <Article  itemname="Apfel" imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT18SPNkehaQjo27-ZH80u919k2MvU7mM7DqmutmEbiViiIZDKA&usqp=CAU"/>
+
       </>
     )
   }
