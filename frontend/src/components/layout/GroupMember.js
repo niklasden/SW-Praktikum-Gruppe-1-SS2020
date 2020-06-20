5e5571de22973e0d94f66003f4b1afaa85c8d4d1
JuliusJacobitz <julius@jacobitz.de>
2020-06-20 16:43:47 +0200

    added group member and group list items and worked on specific group

---
+++ frontend/src/components/layout/GroupMember.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import PropTypes from 'prop-types';
+import Checkbox from "@material-ui/core/Checkbox"
+import { withStyles } from "@material-ui/styles";
+import Box from '@material-ui/core/Box';
+import MaterialIconButton from '@material-ui/core/IconButton';
+import { Grid } from "@material-ui/core";
+
+/**
+ * Displays a group member item in a specific group
+ * 
+ * @author [Julius Jacobitz](https://github.com/JuliusJacobitz)
+ * 
+*/
+
+const styles = theme => ({
+  root: {
+    backgroundColor: '#F2F2F2',
+    width: 'auto',
+    corderRadius: 10,
+    border: 1
+  },
+});
+
+class GroupMember extends Component {
+  render() {
+    return (
+    <Grid 
+      container 
+      direction='row'
+      justify='space-between'
+      alignItems='center'
+      className={this.props.classes.root}
+       >
+        <Grid item xs={3}>
+          <img style={{height:'25px', width: '25px'}} src={this.props.imgsrc}></img>
+        </Grid>
+
+        <Grid item xs={6}>
+          <p style={{color: '#000000', fontSize: 18}}>{this.props.membername}</p>
+        </Grid>
+
+        
+    </Grid>
+    );
+  }
+}
+
+GroupMember.propTypes = {
+  imgsrc: PropTypes.string.isRequired,
+  membername: PropTypes.string.isRequired,
+}
+
+export default withStyles(styles)(GroupMember);
--- frontend/src/components/pages/SpecificGroup.js
+++ frontend/src/components/pages/SpecificGroup.js
@@ -1,35 +1,129 @@
-import React, { Component } from 'react'
+import React, { Component, memo } from 'react'
 import PropTypes from 'prop-types';
 import MaterialIconButton from '@material-ui/core/IconButton';
 import Icon from '@material-ui/icons/Add';
 import { withStyles } from '@material-ui/styles';
 import TextField from '@material-ui/core/TextField';
 
+import MainButton from '../layout/MainButton.js'
+import createPalette from '@material-ui/core/styles/createPalette';
+
+import Typography from '@material-ui/core/Typography';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import GroupMember from '../layout/GroupMember.js'
+import GroupListItem from '../layout/GroupListItem.js'
+
 const styles = theme => ({
   root: {
     backgroundColor: '#fafafa', 
-    borderRadius: 5, 
+    borderRadius: 5,
+    
   },
+  Groupnameheader:{
+    ...theme.typography.button,
+    backgroundColor: '#000000',
+    borderRadius: 5,
+    color: theme.palette.white,
+    textAlign:"center",
+    marginTop:"3%",
+    fontSize: '115%'
+  },
+  accordion:{
+    width: '100%',
+    marginTop:"3%"
+  },
+  heading:{
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  }
 });
 
+const ShoppingLs=[
+  {id:"1",name:"liste 2"},
+  {id:"2",name:"list 2.4"}
+]
+const mem=[
+  {id:"1",name:"Julius"},
+  {id:"2",name:"Niklas"}]
+
 /**
- * Displays an icon button as designed in figma
+ * Bildet eine Spezifische Gruppe ab
  * 
- * @author [Niklas Denneler](https://github.com/)
+ * Beim Aufruf der Specific group muss der name auf den Gruppennamen gesetzt werden
+ * 
  * @author [Julius Jacobitz]()
  * 
- * @property icon (string): the icon name to display, can be either: add, shopping_cart, shopping_cart_outline, shopping_list, shopping_list_outline, checkmark, people
  */
 class SpecificGroup extends Component {
+  renderShoppinglists(){
+  const ShoppingLists = []
+  ShoppingLs.forEach( elem => {
+    ShoppingLists.push(<GroupListItem key={elem.id} Listname={elem.name} ></GroupListItem>)
+})
+return ShoppingLists
+  }
+
+  renderGroupMembers(){
+    const GroupMembers = []
+    mem.forEach( elem => {
+      GroupMembers.push(<GroupMember key={elem.id} imgsrc="" membername={elem.name}></GroupMember>)
+    })
+    return GroupMembers
+  }
+
   render(){
     const { classes } = this.props;
+    
 
     return (
-        <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Group Name" variant="outlined" />
-      </form>
+        <div className={classes.accordion}>
+        {/*<div className={classes.Groupnameheader}>{"Gruppenname"}</div>*/}
+
+        <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>Shopping Lists</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          
+           {this.renderShoppinglists()}
+        
+        </ExpansionPanelDetails>
+        </ExpansionPanel>
+
+{/* Members: ---------------------------------------------------------*/}
 
 
+        <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>Members</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          
+          
+        {this.renderGroupMembers()}
+          
+
+        </ExpansionPanelDetails>
+        </ExpansionPanel>
+
+        </div>
+
+
+      
+    
     )
   }
 }
