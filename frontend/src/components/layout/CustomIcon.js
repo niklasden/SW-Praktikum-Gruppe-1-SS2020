8fa144b7eecf09324ec0f5f23588ef5c49a74db7
JuliusJacobitz <julius@jacobitz.de>
2020-06-21 17:21:05 +0200

    added icons

--- frontend/src/components/layout/CustomIcon.js
+++ frontend/src/components/layout/CustomIcon.js
@@ -6,6 +6,33 @@ import Icon from '@material-ui/core/Icon'
 import appleIcon from '../../icons/apple.svg'
 import milkIcon from '../../icons/milk.svg'
 import breadIcon from '../../icons/bread.svg'
+import beermugIcon from '../../icons/beermug.svg'
+import orangejuiceIcon from '../../icons/orangejuice.svg'
+import teaIcon from '../../icons/tea.svg'
+import wineIcon from '../../icons/wine.svg'
+import colaIcon from '../../icons/cola.svg'
+import waterIcon from '../../icons/water.svg'
+import icecreamIcon from '../../icons/icecream.svg'
+import donutIcon from '../../icons/donut.svg'
+import chipsIcon from '../../icons/chips.svg'
+import popcornIcon from '../../icons/popcorn.svg'
+import bombomsIcon from '../../icons/bomboms.svg'
+import chocolateIcon from '../../icons/chocolate.svg'
+import cakeIcon from '../../icons/cake.svg'
+import cokkiesIcon from '../../icons/cookies.svg'
+import flourIcon from '../../icons/flour.svg'
+import saltpepperIcon from '../../icons/saltpepper.svg'
+import basilIcon from '../../icons/basil.svg'
+import chilliIcon from '../../icons/chilli.svg'
+import garlicIcon from '../../icons/garlic.svg'
+import ketchupIcon from '../../icons/ketchup.svg'
+import mustardIcon from '../../icons/mustard.svg'
+import pizzaIcon from '../../icons/pizza.svg'
+
+
+
+
+
 
 // add your icon here, you can then call it by <CustomIcon name='iconname' />
 // place the file in icons and import it the same way the appleIcon has been imported
@@ -13,7 +40,37 @@ import breadIcon from '../../icons/bread.svg'
 const icons = { 
   apple: appleIcon, 
   milk: milkIcon, 
-  bread: breadIcon, 
+  bread: breadIcon,
+  beermug:beermugIcon,
+  orangejuice:orangejuiceIcon,
+  tea:teaIcon,
+  wine:wineIcon,
+  cola:colaIcon,
+  water:waterIcon,
+  icecream:icecreamIcon,
+  donut:donutIcon,
+  chips:chipsIcon,
+  popcorn:popcornIcon,
+  bomboms:bombomsIcon,
+  chocolate:chocolateIcon,
+  cake:cakeIcon,
+  cookies:cokkiesIcon,
+  flour:flourIcon,
+  saltpepper:saltpepperIcon,
+  basil:basilIcon,
+  chilli:chilliIcon,
+  garlic:garlicIcon,
+  ketchup:ketchupIcon,
+  mustard:mustardIcon,
+  pizza:pizzaIcon,
+  
+
+
+
+
+
+
+  
 }
 
 /**
