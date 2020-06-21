5c217ab454a45cfd8a16271fa437b4b83eb98212
Christpoher Böhm <exsivi.project@gmail.com>
2020-06-21 17:22:00 +0200

    added icons from flaticons

--- frontend/src/components/layout/CustomIcon.js
+++ frontend/src/components/layout/CustomIcon.js
@@ -6,6 +6,22 @@ import Icon from '@material-ui/core/Icon'
 import appleIcon from '../../icons/apple.svg'
 import milkIcon from '../../icons/milk.svg'
 import breadIcon from '../../icons/bread.svg'
+import orangeIcon from '../../icons/orange.svg'
+import strawberryIcon from '../../icons/strawberry.svg'
+import bananaIcon from '../../icons/banane.svg'
+import grapeIcon from '../../icons/grape.svg'
+import cucumberIcon from '../../icons/cucumber.svg'
+import tomatoIcon from '../../icons/tomato.svg'
+import lettuceIcon from '../../icons/lettuce.svg'
+import meatIcon from '../../icons/meat.svg'
+import fishIcon from '../../icons/fish.svg'
+import cheeseIcon from '../../icons/cheese.svg'
+import eggIcon from '../../icons/egg.svg'
+import noodlesIcon from '../../icons/noodles.svg'
+import lipstickIcon from '../../icons/lipstick.svg'
+import yoghurtIcon from '../../icons/yoghurt.svg'
+import chickenIcon from '../../icons/chicken.svg'
+import soapIcon from '../../icons/soap.svg'
 
 // add your icon here, you can then call it by <CustomIcon name='iconname' />
 // place the file in icons and import it the same way the appleIcon has been imported
@@ -14,6 +30,22 @@ const icons = {
   apple: appleIcon, 
   milk: milkIcon, 
   bread: breadIcon, 
+  orange: orangeIcon, 
+  strawberyy: strawberryIcon, 
+  banana: bananaIcon, 
+  grape: grapeIcon, 
+  cucumber: cucumberIcon, 
+  tomato: tomatoIcon, 
+  lettuce: lettuceIcon, 
+  meat: meatIcon, 
+  chicken: chickenIcon, 
+  fish: fishIcon, 
+  cheese: cheeseIcon, 
+  egg: eggIcon, 
+  noodles: noodlesIcon, 
+  lipstick: lipstickIcon,
+  yoghurt: yoghurtIcon, 
+  soap: soapIcon, 
 }
 
 /**
