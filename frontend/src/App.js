6f8cd11f73f178a6ac9c58395c64558bf4e350da
Pascal Illg <60141744+pasillg@users.noreply.github.com>
2020-06-24 12:59:50 +0200

    modified App.js, IconButton and Menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -141,7 +141,6 @@ class App extends React.Component {
     render(){
 	  const { currentUser, appError, authError, authLoading,isNavHidden } = this.state;
 	
-<<<<<<< HEAD
       return (
         <ThemeProvider theme={Theme}>
           {/* Global CSS reset and browser normalization. CssBaseline kickstarts an elegant, consistent, and simple baseline to build upon. */}
@@ -232,92 +231,5 @@ class App extends React.Component {
       );
     }
   }
-=======
-		return (
-			<ThemeProvider theme={Theme}>
-				{/* Global CSS reset and browser normalization. CssBaseline kickstarts an elegant, consistent, and simple baseline to build upon. */}
-				<CssBaseline />
-				<Router basename={process.env.PUBLIC_URL}>
-					<Header user={currentUser} />
-					{
-						// Is a user signed in?
-						// geändert von chris, um im dev prozess den signin zu umgehen, muss wieder 
-						// TODO: muss wieder in currentUser umbenannt werden
-						// Is a user signed in?
-						true ?
-							<>
-								{/* Here should the redirects go */}
-								<Switch>
-									<Route path="/about">
-										<AboutPage />
-									</Route>
-									<Route path="/users">
-										<UsersPage />
-									</Route> 
-									<Route path="/products">
-										<ProductsPage />
-									</Route>
-									<Route path="/create_article">
-										<CreateArticlePage />
-									</Route>
-									<Route path="/retailers">
-										<RetailerPage />
-									</Route>
-									<Route path="/create_retailer">
-										<CreateRetailerPage />
-									</Route>
-									<Route path="/specificgroup">
-										<SpecificGroup/>
-									</Route>
-									<Route path="/Groups">
-										<Groups></Groups>
-									</Route>
-									<Route path="/GroupShoppingList">
-										<GroupShoppingList/>
-									</Route> 
-									<Route path="/settings">
-										<SettingsPage/>
-									</Route>
-									{/* this must always be the last route */}
-									<Route path="/GroupShoppingList">
-										<GroupShoppingList/>
-									</Route>  
-									<Route path="/createGroup">
-										<CreateGroup/>
-									</Route>
-									<Route path="/specificGroup">
-										<SpecificGroup></SpecificGroup>
-									</Route>
-									<Route path="/allGroups">
-										<Groups></Groups>	
-									</Route>
-									
-									{/* this must always be the last route */}
-									<Route path="/">
-										<HomePage />
-									</Route>
-								</Switch>
-							</>
-							:
-							// else show the sign in page
-							<>
-								<Redirect to='/index.html' />
-								<SignIn onSignIn={this.handleSignIn} />
-							</>
-						}
-						<Container>
-							<LoadingProgress show={authLoading} />
-							<ContextErrorMessage error={authError} contextErrorMsg={`Something went wrong during sign in process.`} onReload={this.handleSignIn} />
-							<ContextErrorMessage error={appError} contextErrorMsg={`Something went wrong inside the app. Please reload the page.`} />
-						</Container>
-				{(this.state.isNavHidden) ? null : <BottomNavigation /> } 
-				{/* <BottomNavigation/>  */}
-				{/* Prüfen ob User auf home-page dann menü nicht rendern */}
-				</Router>
-			</ThemeProvider>
-		);
-	}
-}
->>>>>>> d74ae78b7c86265905a56035c8ef9e5fbd3e609a
 
 export default App;
--- frontend/src/components/layout/IconButton.js
+++ frontend/src/components/layout/IconButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import MaterialIconButton from '@material-ui/core/IconButton';
 import Icon from '@material-ui/core/Icon';
