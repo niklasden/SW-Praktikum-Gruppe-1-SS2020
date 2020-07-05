af5697b5a25263d3c75ff66bfe8683f6c07d093e
Pia Schmid <ps123@hdm-stuttgart.de>
2020-07-05 20:38:36 +0200

    started onSave function on ProductListEntry

--- frontend/src/components/layout/ProductListEntry.js
+++ frontend/src/components/layout/ProductListEntry.js
@@ -3,6 +3,11 @@ import Grid from '@material-ui/core/Grid';
 import { withStyles, ButtonBase } from '@material-ui/core';
 import Article from '../layout/Article';
 import { Link } from 'react-router-dom';
+import { push } from "react-router";
+import GroupShoppingList from '../pages/GroupShoppingList';
+import { withRouter } from "react-router-dom";
+
+
 
 
 
@@ -30,34 +35,41 @@ const styles = theme => ({
  */
 
  class ProductListEntry extends Component {
-      onSave(){
-        
-      }
- 
-     render(){
-         const { classes } = this.props
-         return (     
-              <Grid item 
-              className={classes.article}
-              >
-                <Grid item 
-                xs={2.4}
-                direction="row"
-                >
-                  <ButtonBase
-                    onClick
-                    to= 'create_article?query=hello'
-                    query='hello'
-                    to={{
-                      pathname:'create_article', 
-                      state:{name: this.props.name, category: this.props.category}
-                    }}>
-                    <Article itemname={this.props.name} category = {this.props.category} iconName={this.props.iconName}></Article>
-                  </ButtonBase>
-                </Grid>
-              </Grid>
-          )
-     }
-  }
 
- export default withStyles(styles)(ProductListEntry);
+  onSave= () => {
+    this.state= {name: this.props.name, category: this.props.category}
+    console.log(this.props.name, this.props.category)
+    this.props.history.push({
+      pathname: '/GroupShoppingList', 
+      state: {item: this.props.name, ArrCategory: this.props.category}
+    })
+  }
+     
+
+  render(){
+      const { classes } = this.props
+      return (     
+          <Grid item 
+          className={classes.article}
+          >
+            <Grid item 
+            xs={2.4}
+            direction="row"
+            >
+              <ButtonBase
+                onClick={this.onSave}
+                /*to= 'create_article?query=hello'
+                query='hello'
+                to={{
+                  pathname:'create_article', 
+                  state:{name: this.props.name, category: this.props.category}
+                }}*/>
+                <Article itemname={this.props.name} category = {this.props.category} iconName={this.props.iconName}></Article>
+              </ButtonBase>
+            </Grid>
+          </Grid>
+      )
+  }
+}
+
+ export default withRouter(withStyles(styles)(ProductListEntry));
