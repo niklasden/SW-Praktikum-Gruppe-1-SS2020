15b28dce7d6aa8ecf54a2d937a2cfd37a5414935
JuliusJacobitz <julius@jacobitz.de>
2020-07-10 11:11:47 +0200

    added transferierbare struktur + marshalling for group and worked on group mapper

--- src/server/bo/Group.py
+++ src/server/bo/Group.py
@@ -7,10 +7,17 @@ class Group(bo.BusinessObject):
     """
     def __init__(self, g_name="Group_name"):
         super().__init__()
-        self.members =  []
+        self.description = ""
         self.name = g_name
-        self.shopping_lists = []
+       
 
+    def set_description(self,d):
+        self.description = d
+
+    def get_description(self):
+        return self.description
+
+    """    
     def add_member(self, user):
         self.members.append(user)
     
@@ -20,19 +27,20 @@ class Group(bo.BusinessObject):
     
     def get_members(self):
         return self.members
+    """
 
     def get_name(self):
         return self.name
 
     def set_name(self,name):
         self.name = name
-    
+    """
     def get_shopping_lists(self):
-        return self.shopping_lists
+        return self.shopping_list
     
     def add_shopping_list(self,s_list):
-        self.shopping_lists.append(s_list)
-
+        self.shopping_list = s_list
+"""
     def __str__(self):
         return "Groupobject: Name = {0}".format(self.name)
 
