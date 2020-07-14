fd8950fdd46fad161a73362f43616103129d1c15
kevin96e@live.de <Kevin1996->
2020-07-14 14:11:53 +0200

    fixed endless-loop while fetchin groups

--- frontend/src/components/layout/AllGroupsGridList.js
+++ frontend/src/components/layout/AllGroupsGridList.js
@@ -59,6 +59,7 @@ class GroupsGridList extends Component {
         super(props);
         this.state ={
           groupItemss: [],
+          groupsFetched: false
         };
       }
     
@@ -68,6 +69,7 @@ class GroupsGridList extends Component {
         const res = await fetch(Config.apiHost + '/Group/Usergroup/'+ this.props.currentUserID)
         const resjson = await res.json()
         this.setState({groupItemss:resjson})
+        this.setState({groupsFetched: true})
 
       }
       
@@ -81,50 +83,39 @@ class GroupsGridList extends Component {
       
       render(){
         const { classes } = this.props;
-        var groupI = this.state.groupItems
-        if (this.state.groupItemss.length == 0){
+        if (this.state.groupItemss.length == 0 && !this.state.groupsFetched){
           this.fetchGroups()
               }
-        
         if (settingsobj.getGroupID() == 0){
           fetch(Config.apiHost + '/Group/Usergroup/'+ this.props.currentUserID)
           .then(response => response.json())
           .then(data => {if(data.length > 0) {
             settingsobj.setGroupID(1)
           }})
-
+          console.log(this.state.groupsFetched)
         }
         return(
           
             <div className={classes.rootTwo}>
-                <GridList className={classes.gridList} cellHeight={180} cols={2.5}>
-                
-
-                {this.state.groupItemss.map((tile) => (
-
-                  
-                 <GridListTile key={GroupIcon}>
-                     <img src={GroupIcon} alt={tile.name} />
-                     
-                <Link to="" onClick={() => {settingsobj.setGroupID(tile.id);settingsobj.setGroupName(tile.name); alert("Active group set to: " + settingsobj.getGroupName() + "  |  Group ID: "+ settingsobj.getGroupID())}} aria-label={`info about ${tile.title}`} className={classes.icon}>
-                 <GridListTileBar 
-                     title={ tile.name}
-                    classes={{
-                      root: classes.titleBar,
-                     title: classes.title,
-                          }}
-                     
-                            /> </Link>
-                 </GridListTile>
-                 
-                ))}
-
-
+                {this.state.groupItemss.length !== 0 ?
+                  this.state.groupItemss.map((tile) => (
+                <GridList className={classes.gridList} cellHeight={180}>
+                  <GridListTile key={tile.id}>
+                      <img src={GroupIcon} alt={tile.name} />
+                  <Link to="" onClick={() => {settingsobj.setGroupID(tile.id);settingsobj.setGroupName(tile.name); alert("Active group set to: " + settingsobj.getGroupName() + "  |  Group ID: "+ settingsobj.getGroupID())}} aria-label={`info about ${tile.title}`} className={classes.icon}>
+                  <GridListTileBar 
+                      title={ tile.name}
+                      classes={{
+                        root: classes.titleBar,
+                      title: classes.title,
+                            }}
+                      
+                              /> </Link>
+                  </GridListTile>
                 </GridList>
-                
-
-
-
+                  ))
+                  : <div>There are no groups.</div>
+                }
             </div>
         )
       }
