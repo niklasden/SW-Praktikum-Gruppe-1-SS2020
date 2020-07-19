46644b624c5182e3b7520b03eb6bc1a8e680ec75
Christpoher Böhm <exsivi.project@gmail.com>
2020-07-19 22:12:44 +0200

    added max width to page contents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -252,90 +252,94 @@ class App extends React.Component {
 						<CssBaseline />
 						<Router basename={process.env.PUBLIC_URL}>
 							<Header user={currentUser} />
-							{
-								// Is a user signed in?
-								// geändert von chris, um im dev prozess den signin zu umgehen, muss wieder 
-								// TODO: muss wieder in currentUser umbenannt werden
-								// Is a user signed in?
-								currentUser && currentUserID ?  //currentUserID for context 
-							
-									<>
-										{/* Here should the redirects go */}
-										<SettingsContextProvider value={{user:{ name: currentUser.displayName, id:currentUserID } ,currentGroupID:0}}>
-											<Switch>
-												<Route path="/about">
-													<AboutPage />
-												</Route>
-												<Route path="/users">
-													<UsersPage />
-												</Route> 
-												<Route path="/products">
-													<ProductsPage />
-												</Route>
-												<Route path="/create_article">
-													<CreateArticlePage />
-												</Route>
-												<Route path="/favorite_products">
-													<FavoriteArticlesPage />
-												</Route>
-												<Route path="/add_favorite_article">
-													<AddFavoriteArticle />
-												</Route>
-												<Route path="/edit_favorite_article">
-													<EditFavoriteArticle />
-												</Route>
-												<Route path="/retailers">
-													<RetailerPage />
-												</Route>
-												<Route path="/create_retailer">
-													<EditRetailerPage />
-												</Route>
-												<Route path="/specificgroup">
-													<SpecificGroup/>
-												</Route>
-												<Route path="/Groups">
-													<Groups></Groups>
-												</Route>
-												<Route path="/GroupShoppingList">
-													<GroupShoppingList/>
-												</Route> 
-												<Route path="/settings">
-													<SettingsPage/>
-												</Route>
-												<Route path="/PersonalShoppingList">
-													<PersonalShoppingList/>
-												</Route>  
-												<Route path="/createGroup">
-													<CreateGroup/>
-												</Route>
-												<Route path="/specificGroup">
-													<SpecificGroup></SpecificGroup>
-												</Route>
-												<Route path="/allGroups">
-													<Groups></Groups>	
-												</Route>
-												<Route path="/settings-accounts">
-													<AccountsPage />
-												</Route>
-												<Route path='/report' component={() => { window.location = 'http://report.ikaufa.com/'; return null;} }/>
-												{/* this must always be the last route */}
-												<Route path="/">
-												
-													<HomePage currentUserID={this.state.currentUserID} />
-												</Route>
-											</Switch>
-										</SettingsContextProvider>
-									</>
-									:
-									// else show the sign in page
-									<>
-										<Redirect to='/index.html' />
-										<SignIn onSignIn={this.handleSignIn} />
-									</>
-								}
+							<div style={{justifyContent: 'center', display: 'flex'}}>
+								<div style={{maxWidth: 700}}>
+								{
+									// Is a user signed in?
+									// geändert von chris, um im dev prozess den signin zu umgehen, muss wieder 
+									// TODO: muss wieder in currentUser umbenannt werden
+									// Is a user signed in?
+									currentUser && currentUserID ?  //currentUserID for context 
+								
+										<>
+											{/* Here should the redirects go */}
+											<SettingsContextProvider value={{user:{ name: currentUser.displayName, id:currentUserID } ,currentGroupID:0}}>
+												<Switch>
+													<Route path="/about">
+														<AboutPage />
+													</Route>
+													<Route path="/users">
+														<UsersPage />
+													</Route> 
+													<Route path="/products">
+														<ProductsPage />
+													</Route>
+													<Route path="/create_article">
+														<CreateArticlePage />
+													</Route>
+													<Route path="/favorite_products">
+														<FavoriteArticlesPage />
+													</Route>
+													<Route path="/add_favorite_article">
+														<AddFavoriteArticle />
+													</Route>
+													<Route path="/edit_favorite_article">
+														<EditFavoriteArticle />
+													</Route>
+													<Route path="/retailers">
+														<RetailerPage />
+													</Route>
+													<Route path="/create_retailer">
+														<EditRetailerPage />
+													</Route>
+													<Route path="/specificgroup">
+														<SpecificGroup/>
+													</Route>
+													<Route path="/Groups">
+														<Groups></Groups>
+													</Route>
+													<Route path="/GroupShoppingList">
+														<GroupShoppingList/>
+													</Route> 
+													<Route path="/settings">
+														<SettingsPage/>
+													</Route>
+													<Route path="/PersonalShoppingList">
+														<PersonalShoppingList/>
+													</Route>  
+													<Route path="/createGroup">
+														<CreateGroup/>
+													</Route>
+													<Route path="/specificGroup">
+														<SpecificGroup></SpecificGroup>
+													</Route>
+													<Route path="/allGroups">
+														<Groups></Groups>	
+													</Route>
+													<Route path="/settings-accounts">
+														<AccountsPage />
+													</Route>
+													<Route path='/report' component={() => { window.location = 'http://report.ikaufa.com/'; return null;} }/>
+													{/* this must always be the last route */}
+													<Route path="/">
+													
+														<HomePage currentUserID={this.state.currentUserID} />
+													</Route>
+												</Switch>
+											</SettingsContextProvider>
+										</>
+										:
+										// else show the sign in page
+										<>
+											<Redirect to='/index.html' />
+											<SignIn onSignIn={this.handleSignIn} />
+										</>
+									}
 									<LoadingProgress show={authLoading} />
 									<ContextErrorMessage error={authError} contextErrorMsg={`Something went wrong during sign in process.`} onReload={this.handleSignIn} />
 									<ContextErrorMessage error={appError} contextErrorMsg={`Something went wrong inside the app. Please reload the page.`} />
+								</div>
+							</div>
 							<BottomNavigation/> 
 						{/* Prüfen ob User auf home-page dann menü nicht rendern */}
 						</Router>
