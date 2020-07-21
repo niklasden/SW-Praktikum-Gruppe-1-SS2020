fca2864e2dd5779962d2f97f6f523d0907d29557
Pia Schmid <ps123@hdm-stuttgart.de>
2020-07-21 12:48:11 +0200

    cleaned up the code of AddListItem

--- frontend/src/components/layout/AddListItem.js
+++ frontend/src/components/layout/AddListItem.js
@@ -27,7 +27,6 @@ const styles = theme => ({
     marginRight:10, 
     marginBottom: 10
   },
-
   popUp: {
       width: '300px', 
       fontSize: '15px',      
@@ -48,26 +47,24 @@ class AddListItem extends Component {
         item: this.props.item, 
         article_id: this.props.item.id,
         category: this.props.item.category,
-        amount: null, 
-        unit: null, 
-        shoppinglists: [],
-        selected_shoppinglist: null, 
-        retailers: [], 
+        selected_shoppinglist: null,
+        selected_user_id: null, 
         selected_retailer_id: null,
+        amount: null, 
+        unit: null,
+
+        shoppinglists: [],
         users:[],
-        selected_user_id: null,
-        isloading: false,
-
-
+        retailers: [], 
         units: [
           {id: 'g', name: 'g'},
           {id: 'l', name: 'l'},
           {id: 'ml', name: 'ml'},
           {id: 'Stk.', name: 'Stk.'},
           {id: 'Pkg.', name: 'Pkg.'},
-        ]
+        ],
 
-        
+        isloading: false,
     }
      this.saveItem = this.saveItem.bind(this); 
   }
@@ -99,11 +96,9 @@ class AddListItem extends Component {
   setUnit(v) {
     this.setState({unit: v.target.value});
   }
-
   setUser(v) {
     this.setState({selected_user_id: v.target.value});
   }
-
   setAmount(v) {
     this.setState({amount: v.target.value})
   }
@@ -114,13 +109,6 @@ class AddListItem extends Component {
   }
     
   saveItem = () => {
-    console.log(this.state.selected_shoppinglist)
-    console.log(this.state.article_id)
-    console.log(this.state.selected_retailer_id)
-    console.log(this.state.amount)
-    console.log(this.state.unit)
-
-
     let insertedItem = Object.assign(new ListEntryBO(), this.state.item);
     //Insert the parameters of the new ListEnty
     insertedItem.setArticleid(this.state.article_id);
@@ -135,7 +123,6 @@ class AddListItem extends Component {
     insertedItem.setBought(null);
     insertedItem.setName(null);
     insertedItem.setCategory(null);
-
     
     //Sends new ListEntry Object to the API, in case of Error it logs it
     ShoppingAPI.getAPI().insertListEntry(insertedItem).then(() => {console.log(insertedItem); this.props.PressButtonConfirm()}).catch(e => console.log(e))
@@ -146,148 +133,129 @@ class AddListItem extends Component {
 
     const  classes  = this.props.classes
 
-
     return (
 
     <Dialog open={this.props.open} aria-labelledby="alert title" aria-describedby="description"> 
       <DialogTitle id="alert title" style={{textAlign: "center"}}>{"add "+this.state.item.name}</DialogTitle>
       <DialogContent>
-      <Grid
-        container
+
+        <Grid container
         direction='row'
         justify='center'
         alignItems= 'center'
         spacing={4}
         className={classes.popUp}
-      >
-      <Grid item xs={12} >
-      <FormControl
-      className={classes.formControl} 
-      >
-                <InputLabel>SHOPPING LIST</InputLabel>
+        >
+          <Grid item xs={12} >
+            <FormControl
+            className={classes.formControl} 
+            >
+              <InputLabel>SHOPPING LIST</InputLabel>
                 <Select  
-                onChange={(e) => this.setState({selected_shoppinglist: e.target.value})}
-                value={this.state.selected_shoppinglist}>
-                
+                  onChange={(e) => this.setState({selected_shoppinglist: e.target.value})}
+                  value={this.state.selected_shoppinglist}
+                  >
                     {this.state.isloading ?
-                        <div style={{display: 'flex', justifyContent: 'center'}}>                 
-                            <CircularProgress size={25} />               
-                        </div>
-                        :
-                        this.state.shoppinglists.map((item, key) => 
-                            <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
+                      <div style={{display: 'flex', justifyContent: 'center'}}>                 
+                        <CircularProgress size={25}/>               
+                      </div>
+                      :
+                      this.state.shoppinglists.map((item, key) => 
+                        <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
                     )}
                 </Select>
-      </FormControl>
-      </Grid>
+            </FormControl>
+          </Grid>
 
-      <Grid item xs={12} >
-      <FormControl
-      className={classes.formControl} 
-      >
-                <InputLabel>RETAILER</InputLabel>
-                <Select  
+          <Grid item xs={12} >
+            <FormControl
+            className={classes.formControl} 
+            >
+              <InputLabel>RETAILER</InputLabel>
+                <Select
                 onChange={(e) => this.setState({selected_retailer_id: e.target.value})}
-                value={this.state.selected_retailer_id}>
-                
-                    {this.state.isloading ?
-                        <div style={{display: 'flex', justifyContent: 'center'}}>                 
-                            <CircularProgress size={25} />               
-                        </div>
-                        :
-                        this.state.retailers.map((item, key) => 
-                            <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
-                    )}
-                    </Select>
-      </FormControl>
-      </Grid>
+                value={this.state.selected_retailer_id}
+                >
+                  {this.state.isloading ?
+                    <div style={{display: 'flex', justifyContent: 'center'}}>                 
+                      <CircularProgress size={25}/>               
+                    </div>
+                    :
+                    this.state.retailers.map((item, key) => 
+                      <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
+                  )}
+                </Select>
+            </FormControl>
+          </Grid>
 
-      <Grid item xs={12} >
-      <FormControl
-      className={classes.formControl} 
-      >
-                <InputLabel>USER</InputLabel>
+          <Grid item xs={12} >
+            <FormControl
+            className={classes.formControl} 
+            >
+              <InputLabel>USER</InputLabel>
                 <Select  
                 onChange={(e) => this.setState({selected_user_id: e.target.value})}
-                value={this.state.selected_user_id}>
-                
-                    {this.state.isloading ?
-                        <div style={{display: 'flex', justifyContent: 'center'}}>                 
-                            <CircularProgress size={25} />               
-                        </div>
-                        :
-                        this.state.users.map((item, key) => 
-                            <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
-                    )}
-                    </Select>
-      </FormControl>
-      </Grid>
-      <Grid container xs={12}
-      justify = 'center'
-      alignItems= 'center'
-      style={{paddingTop:10, paddingBottom:10}}
-      spacing={4}
+                value={this.state.selected_user_id}
+                >
+                  {this.state.isloading ?
+                    <div style={{display: 'flex', justifyContent: 'center'}}>                 
+                      <CircularProgress size={25} />               
+                    </div>
+                    :
+                    this.state.users.map((item, key) => 
+                      <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
+                  )}
+                </Select>
+            </FormControl>
+          </Grid>
 
-      >
-        <Grid item xs={6} 
-          style={{paddingLeft:25}}
-          justify='center'
+          <Grid container xs={12}
+          justify = 'center'
           alignItems= 'center'
-        >
-          <InputLabel>AMOUNT</InputLabel>
-          <TextField onChange={this.setAmount.bind(this)} value={this.state.amount}>
-          </TextField>
+          style={{paddingTop:10, paddingBottom:10}}
+          spacing={4}
+          >
+            <Grid item xs={6} 
+            style={{paddingLeft:25}}
+            justify='center'
+            alignItems= 'center'
+            >
+              <InputLabel>AMOUNT</InputLabel>
+                <TextField 
+                onChange={this.setAmount.bind(this)} 
+                value={this.state.amount}>
+                </TextField>
+            </Grid>
+
+            <Grid item xs={6}
+            style={{paddingRight:35}}
+            justify='center'
+            alignItems= 'center'
+            >
+              <FormControl style={{width: '100%'}}>
+                <InputLabel>UNIT</InputLabel>
+                <Select 
+                onChange={(e) => this.setState({unit: e.target.value})}
+                value = {this.state.unit}>
+                  {
+                    this.state.units.map((element) => {
+                      return <MenuItem value = {element.id}>{element.name}</MenuItem>
+                    })
+                  }
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
         </Grid>
-
-        <Grid item xs={6}
-        style={{paddingRight:35}}
-        justify='center'
-        alignItems= 'center'
-        >
-          {/**
-          <FormControl style={{width: '100%'}}>
-                    <InputLabel>UNIT</InputLabel>
-                    <Select 
-                      onChange={this.setUnit.bind(this)}
-                      value = {this.state.unit}
-                    >
-                        <MenuItem value={'kg'}>Kg</MenuItem>
-                        <MenuItem value={'g'}>g</MenuItem>
-                        <MenuItem value={'l'}>l</MenuItem>
-                        <MenuItem value={'ml'}>ml</MenuItem>
-                        <MenuItem value={'Stk.'}>Stk.</MenuItem>
-                        <MenuItem value={'Pkg.'}>Pkg.</MenuItem>
-                    </Select>
-          </FormControl>
-        */}
-
-          <FormControl style={{width: '100%'}}>
-                    <InputLabel>UNIT</InputLabel>
-                    <Select 
-                      onChange={(e) => this.setState({unit: e.target.value})}
-                      value = {this.state.unit}>
-                        {
-                          this.state.units.map((element) => {
-                          return <MenuItem value = {element.id}>{element.name}</MenuItem>
-                          })
-                        }
-                    </Select>
-          </FormControl>
-
-        </Grid>
-      </Grid>
-
-
-      </Grid>
-
-      <DialogActions style={{justifyContent: "center"}}>
-        <Button color="primary" onClick={() => this.saveItem()}>
-          SAVE
-        </Button>
-        <Button onClick={this.props.PressButtonBack} color="primary" autoFocus>
-          BACK
-        </Button>
-      </DialogActions>
+        
+        <DialogActions style={{justifyContent: "center"}}>
+          <Button color="primary" onClick={() => this.saveItem()}>
+            SAVE
+          </Button>
+          <Button onClick={this.props.PressButtonBack} color="primary" autoFocus>
+            BACK
+          </Button>
+        </DialogActions>
       </DialogContent>
     </Dialog>
     )
@@ -296,7 +264,6 @@ class AddListItem extends Component {
 AddListItem.propTypes = {
   onChange: PropTypes.func,
   PressButtonBack: PropTypes.func,
-  PressButtonConfirm: PropTypes.func,
 }
 
 export default withStyles(styles)(AddListItem);
