4a3885afc242092ed84e9dc68503a92ca5fcbb74
Kevin Eberhardt <kevin96e@live.de>
2020-07-26 00:09:45 +0200

    modified config

--- frontend-report-client/src/config.js
+++ frontend-report-client/src/config.js
@@ -1,7 +1,8 @@
 const Config = {
   // backend
   apiHost: 'http://backend.ikaufa.com/shopping'
-  // apiHost: 'http://localhost:5000/shopping'
+  // backend local
+  // apiHost: 'http:localhost:5000/shopping'
 }
 
 export { Config }
